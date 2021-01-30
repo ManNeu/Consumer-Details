ee50eccf122bb64d23f523c0988707a14e6763d9
Manish Neupane <mrnish.are@gmail.com>
2021-01-30 20:29:39 +1100

    functionality to add consumer details in consumers route is defined

--- routes/consumers.js
+++ routes/consumers.js
@@ -27,9 +27,56 @@ router.get("/", auth, async (req, res) => {
 
 //add consumer details with post
 
-router.post("/", (req, res) => {
-  res.send("add consumer details");
-});
+router.post(
+  "/",
+  [
+    //from auth we can get req.user
+    auth,
+    body("first_name", "first_name is required").notEmpty(),
+    body("last_name", "last_name is required").notEmpty(),
+    body("email", "email is required").notEmpty(),
+    body("phone", "phone is required").notEmpty(),
+    body("address", "address is required").notEmpty(),
+    body("symptoms", "symptoms is required").notEmpty(),
+    body("travel_history", "travel_history is required").notEmpty(),
+    body("type", "type is required").notEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      first_name,
+      last_name,
+      email,
+      phone,
+      address,
+      symptoms,
+      travel_history,
+      type,
+    } = req.body;
+    try {
+      const newConsumer = new Consumer({
+        first_name,
+        last_name,
+        email,
+        phone,
+        address,
+        symptoms,
+        travel_history,
+        type,
+        user: req.user.id,
+      });
+
+      const consumer = await newConsumer.save();
+      res.json(consumer);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("server error");
+    }
+  }
+);
 
 //update consumer details with put and need id of the consumer that we updating
 
