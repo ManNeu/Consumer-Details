c67bf7f8ab0234d1e169bae4327703f6ae8720d7
Manish Neupane <mrnish.are@gmail.com>
2021-01-30 13:01:15 +1100

    User model defined, users route validation checked and data succesfully saved with hash  password

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,61 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+
+const { body, validationResult } = require("express-validator");
+
+const User = require("../models/User");
 
 // we are sending post request to register our store
 // this is a public route and anyone can have access
 // this route is created for the registeration process
-router.post("/", (req, res) => {
-  res.send("we are registering our store");
-});
+router.post(
+  "/",
+
+  body("first_name", "first_name is required").not().isEmpty(),
+  body("last_name", "last_name is required").not().isEmpty(),
+  body("email", "please enter a valid email").isEmail(),
+  body("password", "Password must be minimum 6 character ").isLength({
+    min: 6,
+  }),
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    //deconstructing req.body
+    const { first_name, last_name, email, password } = req.body;
+
+    //finding user by email and checking if it already exist
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({
+          msg: "User already exist",
+        });
+      }
+      //if user doesnot exist we will create new user using User model
+      user = new User({
+        first_name,
+        last_name,
+        email,
+        password,
+      });
+      //we will hash the password before saving to database with bcrypt
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      //saving the user
+      await user.save();
+      res.send("saved in database");
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("server error");
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,35 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
+//init middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.get("/", (req, res) => res.json({ msg: "checking server" }));
+// const MONGODB_URI = ""
+// const mongoURI = "mongodb+srv://trilogy:trilogy@cluster0.u8m9s.mongodb.net/mernlogin?retryWrites=true&w=majority";
+
+// // Connect to MongoDB
+// mongoose.connect( MONGODB_URI || mongoURI, {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// })
+// .then(() => console.log("MongoDB successfully connected"))
+// .catch(err => console.log(err));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/consumers";
+
+mongoose
+  .connect(MONGODB_URI || "mongodb://localhost/consumers", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch((err) => console.log(err));
 
 //requiring routes
 app.use("/api/users", require("./routes/users"));
