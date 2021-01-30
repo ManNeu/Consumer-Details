d3af050f7d0d837bc4dc31bfb88232fbe510ea9f
Manish Neupane <mrnish.are@gmail.com>
2021-01-30 17:56:14 +1100

    auth.js has now email and password from User model and also validation for both Username and Password is done

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const { body, validationResult } = require("express-validator");
+
+const User = require("../models/User");
 
 //need two routes get the logged in user and login user and get token
 
@@ -9,8 +16,57 @@ router.get("/", (req, res) => {
 });
 
 //Post route to post info and looged in
-router.post("/", (req, res) => {
-  res.send("log in user");
-});
+router.post(
+  "/",
+  //express validation as second parameter
+  body("email", "Please enter a valid email").isEmail(),
+  body("password", "password is required").exists(),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+    //deconstructing email and password from req.body
+    const { email, password } = req.body;
+
+    //using findOne function of mongoose to look for email
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({
+          msg: "Invalid credentials",
+        });
+      }
+      //using bcrypt.compare to validate password
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({
+          msg: "invalid credentials",
+        });
+      }
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        process.env.jwtSecret,
+        {
+          expiresIn: 31556926,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("server error");
+    }
+  }
+);
 
 module.exports = router;
