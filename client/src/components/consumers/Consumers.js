5095f320e98c6908a2d49db812728cff61a2f09a
Manish Neupane <mrnish.are@gmail.com>
2021-02-04 20:36:16 +1100

    CSSTransition use to add to make the transition of added, edited, filtered consumer information

--- client/src/components/consumers/Consumers.js
+++ client/src/components/consumers/Consumers.js
@@ -1,6 +1,8 @@
 import React, { Fragment, useContext } from "react";
 import ConsumerContext from "../../context/consumer/consumerContext";
 import ConsumerItem from "./ConsumerItem";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
 const Consumers = () => {
   //having access data that's in consumerContext
   const consumerContext = useContext(ConsumerContext);
@@ -12,13 +14,19 @@ const Consumers = () => {
   }
   return (
     <Fragment>
-      {filtered !== null
-        ? filtered.map((consumer) => (
-            <ConsumerItem key={consumer.id} consumer={consumer} />
-          ))
-        : consumers.map((consumer) => (
-            <ConsumerItem key={consumer.id} consumer={consumer} />
-          ))}
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map((consumer) => (
+              <CSSTransition key={consumer.id} timeout={500} className="item">
+                <ConsumerItem key={consumer.id} consumer={consumer} />
+              </CSSTransition>
+            ))
+          : consumers.map((consumer) => (
+              <CSSTransition key={consumer.id} timeout={500} className="item">
+                <ConsumerItem key={consumer.id} consumer={consumer} />
+              </CSSTransition>
+            ))}
+      </TransitionGroup>
     </Fragment>
   );
 };
