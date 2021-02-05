cc02b42d25f605095034799f9e1b8eec6e16a0b1
Manish Neupane <mrnish.are@gmail.com>
2021-02-05 19:47:52 +1100

    register component with form where user can submit details, also use as Route

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+
+const Register = () => {
+  const [user, setUser] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { first_name, last_name, email, password, password2 } = user;
+
+  const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submit register");
+  };
+
+  return (
+    <div className="form-container">
+      <h1>
+        Account <span className="text-primary">Register</span>
+      </h1>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="first_name">First Name</label>
+          <input
+            type="text"
+            name="first_name"
+            value={first_name}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="last_name">Last Name</label>
+          <input
+            type="text"
+            name="last_name"
+            value={last_name}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" value={email} onChange={onChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password2">Confirm Password</label>
+          <input
+            type="password"
+            name="password2"
+            value={password2}
+            onChange={onChange}
+          />
+        </div>
+        <input
+          type="submit"
+          value="Register"
+          className="btn btn-primary btn-block"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Register;
